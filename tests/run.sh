6fb0bd377a41a294e3eba0bf6d2bca285d42adc6
tushushu <xmaseve1984@gmail.com>
2022-07-12 21:25:26 +0800

    test scripts

---
+++ tests/run.sh
@@ -0,0 +1,22 @@
+EXIT_STATUS=0
+
+echo "Running clippy..."
+cargo clippy --manifest-path ../mmr3/Cargo.toml -- -D warnings|| EXIT_STATUS=$?
+echo "\n"
+
+echo "Running unit tests..."
+pytest ../tests|| EXIT_STATUS=$?
+echo "\n"
+
+echo "Running flake8..."
+flake8 ../tests|| EXIT_STATUS=$?
+flake8 ../mmr3/python|| EXIT_STATUS=$?
+echo "\n"
+
+echo "Running mypy..."
+mypy ../tests|| EXIT_STATUS=$?
+mypy ../mmr3/python|| EXIT_STATUS=$?
+echo "\n"
+
+echo "Exit status code $EXIT_STATUS!" 
+exit $EXIT_STATUS
