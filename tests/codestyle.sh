dd3e1d7406a085cb4da0755b926d8934687d34d8
tushushu <xmaseve1984@gmail.com>
2022-08-20 10:50:48 +0800

    fix codestyle check

--- tests/codestyle.sh
+++ tests/codestyle.sh
@@ -4,10 +4,6 @@ echo "Running clippy..."
 cargo clippy --manifest-path ../mmr3/Cargo.toml -- -D warnings|| EXIT_STATUS=$?
 echo "\n"
 
-echo "Running unit tests..."
-pytest ../tests|| EXIT_STATUS=$?
-echo "\n"
-
 echo "Running flake8..."
 flake8 ../tests|| EXIT_STATUS=$?
 flake8 ../mmr3/python|| EXIT_STATUS=$?
